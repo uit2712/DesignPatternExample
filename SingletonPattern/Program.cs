435ce6d680882401c7134004c77e3487b4c9ba77
uit2712 <phamquangvi456@gmail.com>
2019-09-16 10:59:43 +0700

    Add project files.

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton1 = Singleton.Instance();
+            Singleton singleton2 = Singleton.Instance();
+
+            singleton1.HelloThere("Songoku");
+            singleton2.HelloThere("Doraemon");
+            Console.WriteLine(Singleton.GetTotalInstances());
+        }
+    }
+}
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Singleton
+    {
+        protected static Singleton _instance = null;
+        protected static int _totalInstances = 0;
+        
+        protected Singleton() { _totalInstances++; }
+
+        public static Singleton Instance()
+        {
+            if (_instance == null)
+                _instance = new Singleton();
+
+            return _instance;
+        }
+
+        public static int GetTotalInstances()
+        {
+            return _totalInstances;
+        }
+
+        public void HelloThere(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+    }
+}
